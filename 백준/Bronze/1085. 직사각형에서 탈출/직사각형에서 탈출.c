842457bf239e7d1813d63bf0fb24cfa230d830d4
Chanhyukk <150499792+Chanhyukk@users.noreply.github.com>
2024-01-12 20:21:26 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int main() {
+    int x, y, w, h;
+    scanf("%d %d %d %d", &x, &y, &w, &h);
+
+    int min_distance = min(min(x, w - x), min(y, h - y));
+    printf("%d\n", min_distance);
+
+    return 0;
+}
