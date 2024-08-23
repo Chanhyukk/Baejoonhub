7cf2b55757ef853dcdc64c344a50aea7c71d826e
Chanhyukk <150499792+Chanhyukk@users.noreply.github.com>
2024-08-23 13:34:39 +0900

    [Silver I] Title: 팩토리얼5, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1564. 팩토리얼5/팩토리얼5.c
+++ 백준/Silver/1564. 팩토리얼5/팩토리얼5.c
@@ -1,40 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-	long long N;
-
-	long long fact = 1;
-	scanf("%lld", &N);
-	for (long long i = 0; i < N; i++)
-	{
-		fact = fact * (N - i);
-		while (fact % 10 == 0)
-		{
-			fact = fact / 10;
-		}
-		fact = fact % 1000000000000;
-	}
-
-
-	fact = fact % 100000;
-	
-if (fact >= 10000) {
-        printf("%lld", fact);
+int main() {
+    long long n;
+    long long num = 1;
+    scanf("%lld", &n);
+    for (long long i = 0; i < n; i++) {
+        num = num * (n - i);
+        while (num % 10 == 0) {
+            num = num / 10;
+        }
+        num = num % 1000000000000;
     }
-    else if (fact >= 1000) {
-        printf("0%lld", fact);
+    num = num % 100000;
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        arr[4 - i] = num % 10;
+        num /= 10;
     }
-    else if (fact >= 100) {
-        printf("00%lld", fact);
+    for (int i = 0; i < 5; i++) {
+        printf("%d", arr[i]);
     }
-    else if (fact >= 10) {
-        printf("000%lld", fact);
-    }
-    else {
-        printf("0000%lld", fact);
-    }
-
-
-	return 0;
+    return 0;
 }
